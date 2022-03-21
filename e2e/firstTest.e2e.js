f54d10a4dbf534a7a9cb348d82b53f03e917bc5f
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-03-21 16:14:22 -0300

    chore(Config Detox - tests E2E): created test e2e

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,23 +1,27 @@
-describe('My first test', () => {
-  beforeAll(async () => {
-    await device.launchApp();
-  });
+describe("My first test", () => {
+    beforeAll(async () => {
+        await device.launchApp();
+    });
 
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+    beforeEach(async () => {
+        await device.reloadReactNative();
+    });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
+    it("should have welcome screen", async () => {
+        await expect(element(by.id("welcome"))).toBeVisible();
+    });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
+    it("check register a new skill", async () => {
+        const inputNewSkill = await element(by.id("input-new"));
+        const buttonAdd = await element(by.id("button-add"));
+        const flatListSkills = await element(by.id("flat-list-skills"));
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
+        await inputNewSkill.tap();
+        await inputNewSkill.typeText("React Native");
+        await buttonAdd.tap();
+
+        await flatListSkills.tap;
+
+        await expect(flatListSkills).toBeVisible();
+    });
 });
